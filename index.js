b5f5213955ae7832c2d2acfb3ae0de8108c4f38a
Mario <m_reder10@cs.uni-kl.de>
2017-04-28 17:43:15 +0200

    loadSaveSync

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const Tnl = require("./tnl");
 
 module.exports = {
     loadSave: loadSave,
+    loadSaveSync: loadSaveSync,
     loadImage: loadImage
 };
 
@@ -16,13 +17,23 @@ async function loadSave(pathToSave) {
         pathToSave = path.resolve(pathToSave);
         if (!fs.existsSync(pathToSave)) throw new Error(`No such folder exists:\n${pathToSave}`);
         fs.access(pathToSave, fs.constants.R_OK | fs.constants.W_OK, (err) => {
-            if (err) throw new Error("Please close you emuulator before executing your script");
+            if (err) throw new Error("Please close your emulator before executing your script");
         });
         fs.readFile(path.resolve(`${pathToSave}/save.dat`), (err, data) => {
             if (err) throw err;
             resolve(new Save(pathToSave, data));
         });
     });
+}
+
+function loadSaveSync(pathToSave) {
+    pathToSave = path.resolve(pathToSave);
+    if (!fs.existsSync(pathToSave)) throw new Error(`No such folder exists:\n${pathToSave}`);
+    fs.access(pathToSave, fs.constants.R_OK | fs.constants.W_OK, (err) => {
+        if (err) throw new Error("Please close your emulator before executing your script");
+    });
+    let data = fs.readFileSync(path.resolve(`${pathToSave}/save.dat`));
+    return new Save(pathToSave, data);
 }
 
 function loadImage(pathToFile) {
