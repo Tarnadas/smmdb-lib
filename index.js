52e8db64e1737c33545b9151bfde4567b4426df3
Mario <m_reder10@cs.uni-kl.de>
2017-05-12 14:46:45 +0200

    babel test

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
-/*let isProd = true;
-try {
-    require("babel-polyfill");
-} catch (err) {
-    isProd = false;
-}*/
-//if (process.env.NODE_ENV === 'production') {
-/*if (isProd) {
-    var smm = require("./legacy/main.js");
-    module.exports = {
-        loadSave: smm.loadSave,
-        loadSaveSync: smm.loadSaveSync,
-        loadImage: smm.loadImage,
-        loadCourse: smm.loadCourse,
-        loadCourseSync: smm.loadCourseSync
-    };
-} else {*/
-    //var smm = require("./build/main.js");
-import * as smm from "./src/main";
-    module.exports = {
-        loadSave: smm.loadSave,
-        loadSaveSync: smm.loadSaveSync,
-        loadImage: smm.loadImage,
-        loadCourse: smm.loadCourse,
-        loadCourseSync: smm.loadCourseSync
-    };
-//}
+import Promise from "bluebird"
+
+import fs from "fs"
+import path from "path"
+
+import Save from "./src/save"
+import Tnl from "./src/tnl"
+
+export {
+    loadSave,
+    loadSaveSync,
+    loadImage
+}
+export {
+    createCourse,
+    createCourseSync
+} from "./src/course"
+
+async function loadSave(pathToSave) {
+    return new Promise((resolve) => {
+        pathToSave = path.resolve(pathToSave);
+        if (!fs.existsSync(pathToSave)) throw new Error(`No such folder exists:\n${pathToSave}`);
+        fs.access(pathToSave, fs.constants.R_OK | fs.constants.W_OK, (err) => {
+            if (err) throw new Error("Please close your emulator before executing your script");
+        });
+        fs.readFile(path.resolve(`${pathToSave}/save.dat`), (err, data) => {
+            if (err) throw err;
+            resolve(new Save(pathToSave, data));
+        });
+    });
+}
+
+function loadSaveSync(pathToSave) {
+    pathToSave = path.resolve(pathToSave);
+    if (!fs.existsSync(pathToSave)) throw new Error(`No such folder exists:\n${pathToSave}`);
+    fs.access(pathToSave, fs.constants.R_OK | fs.constants.W_OK, (err) => {
+        if (err) throw new Error("Please close your emulator before executing your script");
+    });
+    let data = fs.readFileSync(path.resolve(`${pathToSave}/save.dat`));
+    return new Save(pathToSave, data);
+}
+
+function loadImage(pathToFile) {
+    return new Tnl(pathToFile);
+}
