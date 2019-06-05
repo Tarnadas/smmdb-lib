65d61f708651da9f58ef143fc46e7de879f683a2
Mario Reder <mreder1289@gmail.com>
2019-06-05 08:06:51 +0200

    fix travis ci

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 touch ~/.npmrc
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN"
 wasm-pack build
 wasm-pack pack
 wasm-pack publish
