915b5d961a76edfa8e161822ffb903360b021ae1
Mario Reder <mreder1289@gmail.com>
2019-06-06 07:06:23 +0200

    fix travis ci

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,5 @@ echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN"
 wasm-pack build
 wasm-pack pack
 wasm-pack publish
+npm unpublish cemu-smm@$(npx -c 'echo "$npm_package_version"')
 npm dist-tag add cemu-smm@$(npx -c 'echo "$npm_package_version"') next
