93bd86c419f460591f4659c8d354ab3497b25203
Mario Reder <mreder1289@gmail.com>
2019-07-25 17:32:18 +0200

    ci: use install cache

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,4 @@ echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN"
 wasm-pack build
 wasm-pack pack
-wasm-pack publish
-npm unpublish cemu-smm@$(npx -c 'echo "$npm_package_version"')
-npm dist-tag add cemu-smm@$(npx -c 'echo "$npm_package_version"') next
+wasm-pack publish --tag next
--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-git clone https://github.com/Tarnadas/wasm-pack.git
-cd wasm-pack
-cargo install --path . --force
+command -v wasm-pack >/dev/null 2>&1 || {
+    git clone https://github.com/Tarnadas/wasm-pack.git
+    cd wasm-pack
+    cargo install --path . --force
+}
