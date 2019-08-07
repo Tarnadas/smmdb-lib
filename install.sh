920f4fce073afabec0954eeca100e1bf1ccbfe56
Mario Reder <mreder1289@gmail.com>
2019-08-07 08:15:16 +0200

    ci build improvement and dependency update

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 command -v wasm-pack >/dev/null 2>&1 || {
     git clone https://github.com/Tarnadas/wasm-pack.git
     cd wasm-pack
-    cargo install --path . --force
+    cargo install --path .
 }
