7f2bf5b591ab4bbc349f0319c572a407759de87f
Mario Reder <mreder1289@gmail.com>
2020-10-05 07:51:54 +0200

    ci: fix crates publish

--- crate_deploy.sh
+++ crate_deploy.sh
@@ -11,9 +11,8 @@ sed -E "s/${search}/${replace}/g" package.json > package.tmp.json
 mv package.tmp.json package.json
 
 # Crates.io login
-touch ~/.npmrc
 echo "[registry]" >> ~/.cargo/credentials
 echo "token = \"$CARGO_CREDENTIALS\"" >> ~/.cargo/credentials
 
 # Crates.io publish
-cargo publish
+cargo publish --no-verify
