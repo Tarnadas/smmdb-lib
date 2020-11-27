200b46168fd33b48c23d4d61bae1c0fdad798aca
Mario Reder <mreder1289@gmail.com>
2020-11-27 07:05:04 +0100

    shrink wasm size

--- src/lib.rs
+++ src/lib.rs
@@ -56,12 +56,7 @@ cfg_if! {
         pub fn setup_panic_hook() {
             console_error_panic_hook::set_once();
         }
-    }
-}
 
-cfg_if! {
-    if #[cfg(feature = "wee_alloc")] {
-        extern crate wee_alloc;
         #[global_allocator]
         static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
     }
